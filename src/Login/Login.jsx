dcca5ba3ae8aabf9f12baab0aaa076f55d2585a5
himel2535 <monwarhossanhimel@gmail.com>
2025-10-16 02:12:23 +0600

    Add Sign in user using context API

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { use } from "react";
 import { Link } from "react-router";
+import { AuthContext } from "../contexts/AuthContext/AuthContext";
 
 const Login = () => {
+  const { signUser } = use(AuthContext);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    signUser(email, password)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col ">
@@ -10,7 +28,8 @@ const Login = () => {
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <div className="card-body">
-            <form>
+            {/* ---Form--- */}
+            <form onSubmit={handleLogin}>
               {" "}
               <fieldset className="fieldset">
                 <label className="label">Email</label>
--- src/contexts/AuthContext/AuthProvider.jsx
+++ src/contexts/AuthContext/AuthProvider.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { AuthContext } from "./AuthContext";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { auth } from "../../firebase.init";
 
 const AuthProvider = ({ children }) => {
@@ -8,8 +12,23 @@ const AuthProvider = ({ children }) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
+  const signUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // ---Get current user info--
+  onAuthStateChanged(auth, (currentUser) => {
+    
+    if (currentUser) {
+      console.log("Inside if ", currentUser);
+    } else {
+      console.log("OutSide if", currentUser);
+    }
+  });
+
   const authInfo = {
     createUser,
+    signUser,
   };
   return <AuthContext value={authInfo}>{children}</AuthContext>;
 };
